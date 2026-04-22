7edc6a05c2d2ee4cbb54aef57051520957aa063c
ankurraj2606 <ankurraj2606@gmail.com>
2026-04-22 12:45:37 +0530

    handling the multiple routes nested routes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,82 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-//request and response handler for different routes 
+//request and response handler for different routes
 
 // app.use('/',(req, res)=>{
 //     res.send('Hello World from root route');
 // })
 
-app.get('/user',(req, res)=>{
-    res.send({name:'Ankur', age: 25, city: 'Delhi' });
-})
+// app.get('/user',(req, res)=>{
+//     res.send({name:'Ankur', age: 25, city: 'Delhi' });
+// })
 
-app.post('/user',(req, res)=>{
-    res.send({message: 'User created successfully'});
-})
+// app.post('/user',(req, res)=>{
+//     res.send({message: 'User created successfully'});
+// })
 
-app.delete('/user',(req, res)=>{
-    res.send({message: 'User deleted successfully'});
-})  
+// app.delete('/user',(req, res)=>{
+//     res.send({message: 'User deleted successfully'});
+// })
 
-app.use('/test',(req, res)=>{
-    res.send('Hello World from test route');
-})
+// app.use('/test',(req, res)=>{
+//     res.send('Hello World from test route');
+// })
 
-app.use('/profile',(req, res)=>{
-    res.send('Hello World from profile route');
-})
+// app.use('/profile',(req, res)=>{
+//     res.send('Hello World from profile route');
+// })
 
-app.use('/data',(req, res)=>{
-    res.send('Hello World from data route');
-})
+// app.use('/data',(req, res)=>{
+//     res.send('Hello World from data route');
+// })
 
-app.listen(3030,()=>{
-    console.log('Server is running on port 3030');
-})
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("1st Route handler");
+//     next();
+//     res.send("Response for route handler 1.");
+//   },
+//   (req, res, next) => {
+//     console.log("2nd Route handler");
+//     next();
+//     res.send("Response for route handler 2.");
+//   },
+//   (req, res, next) => {
+//     console.log("2nd Route handler");
+//     next();
+//     res.send("Response for route handler 3.");
+//   },
+//   (req, res, next) => {
+//     console.log("2nd Route handler");
+//     res.send("Response for route handler 4.");
+//   },
+// );
+
+app.use(
+  "/user",
+  [(req, res, next) => {
+    console.log("1st Route handler");
+    next();
+    res.send("Response for route handler 1.");
+  },
+  (req, res, next) => {
+    console.log("2nd Route handler");
+    next();
+    res.send("Response for route handler 2.");
+  },
+  (req, res, next) => {
+    console.log("2nd Route handler");
+    next();
+    res.send("Response for route handler 3.");
+  },
+  (req, res, next) => {
+    console.log("2nd Route handler");
+    res.send("Response for route handler 4.");
+  },]
+);
+
+app.listen(3030, () => {
+  console.log("Server is running on port 3030");
+});
